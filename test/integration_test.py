b6cb05753a23b1d229820d7134d7bafc7da50d12
Aviel Yosef <ayosef@redhat.com>
2020-10-14 08:46:18 +0300

    Fix integration test to work with greedy algorithm

--- test/integration_test.py
+++ test/integration_test.py
@@ -1,7 +1,7 @@
 import random
 import time
 from strategies.complete_search import CompleteStrategy
-from strategies.dp import DP
+from strategies.greedy import Greedy
 
 NUM_OF_JOBS = 8
 NUM_OF_TESTS = 10000
@@ -15,24 +15,24 @@ for test_number in range(1, NUM_OF_TESTS + 1):
 
     complete_search_strategy = CompleteStrategy(
         "Complete Search Strategy", a_array, b_array, NUM_OF_JOBS)
-    dp_strategy = DP("DP Strategy", a_array, b_array, NUM_OF_JOBS)
+    greedy_strategy = Greedy("Greedy Strategy", a_array, b_array, NUM_OF_JOBS)
 
     start = time.process_time()
     complete_search_strategy.solve()
     mid = time.process_time()
-    dp_strategy.solve()
+    greedy_strategy.solve()
     end = time.process_time()
 
-    dp_correct_order = dp_strategy.correct_order
+    greedy_correct_order = greedy_strategy.correct_order
     cs_correct_order = complete_search_strategy.correct_order
 
     print(f"{complete_search_strategy.name}: {cs_correct_order}")
-    print(f"{dp_strategy.name}: {dp_correct_order}")
+    print(f"{greedy_strategy.name}: {greedy_correct_order}")
     print(f"Process time of {complete_search_strategy.name}: {mid-start}")
-    print(f"Process time of {dp_strategy.name}: {end-mid}")
+    print(f"Process time of {greedy_strategy.name}: {end-mid}")
 
-    if dp_strategy.cmax == complete_search_strategy.cmax and \
-        dp_strategy.correct_order == complete_search_strategy.correct_order:
+    if greedy_strategy.cmax == complete_search_strategy.cmax and \
+        greedy_strategy.correct_order == complete_search_strategy.correct_order:
         SUCCESS += 1
         print(f"Test #{test_number}: \033[92m\33[1mSUCCESS\33[0m")
     else:
